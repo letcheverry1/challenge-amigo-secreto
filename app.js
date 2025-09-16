34a5f4861f18bd136ef3da5cdbc78a521a3bfd20
Lautaro Etcheverry <lautaroeetcheverry@gmail.com>
2025-09-15 21:15:04 -0300

    Array

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Array donde almacenaremos los nombres
+let amigos = [];
+
