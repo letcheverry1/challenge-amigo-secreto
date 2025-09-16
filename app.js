21cda7df4fdd2be029ff5dea914b42e72a92bb1c
Lautaro Etcheverry <lautaroeetcheverry@gmail.com>
2025-09-16 12:44:55 -0300

    COnfiguracion de todos los archivos

--- app.js
+++ app.js
@@ -2,41 +2,87 @@
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
 // Array donde almacenaremos los nombres
-let amigos = [];
-
-// Funcion que permite sustituir texte de html
-function asignarTextoElem (elemento, texto){
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-    return;
-}
+let nombres = []; 
 
 //Listado de nombre desde el boton añadir
 
 function agregarAmigo (){
-   let nomAmigo= document.getElementById("amigo").value;
+   let newAmigo= document.getElementById("amigo").value;
    
-   if (nomAmigo === ""){
-    alert ("El nombre ingresado no corresponde.");
+   
+   if (newAmigo === ""){
+    alert ("Ingrese un nombre.");
 
    } else{
-    nombres.push(nomAmigo);
-    console.log (nombres);
-   }
-
-   const lista = document.getElementById ("lista");
-   const elementoNuevo = document.getElementById ("li");
-   elementoNuevo.textContent = nomAmigo;
-   lista.appendChild(elementoNuevo);
-   borrarCampo ();
-   
+    if (/\d/. test(newAmigo)){
+        alert ("NO pongas numeros");
+        borrarCampo();
+    } else {
+        if (nombres.includes(newAmigo)){
+            alert("Este nombre se repite en la lista. Escriba uno nuevo.");
+            borrarCampo();
+    }   else  {
+         nombres.push(newAmigo);
+         borrarCampo();
+         mostrarAmigos();
+        }
+    } 
+ } 
 }
+   
+   
+    
+
 
 //Funcion para borrar el campo luego de poner el nombre
 
 function borrarCampo (){
+    document.getElementById ("resultado").textContent = "";
     document.querySelector ("#amigo").value = "";
 }
 
+// Funcion que muestra en una lista la cantidad de amigos
+// Al actualizar la pagina se limpia la lista
 
+function mostrarAmigos(){
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    
+    for (let i=0; i<nombres.length; i++){
+        let cadaAmigo = document.createElement("li");
+        cadaAmigo.textContent = nombres[i];
+        listaAmigos.appendChild(cadaAmigo);
+    }
+}
+
+function sortearAmigo (){
+
+    if (nombres.length === 0){
+        alert("No hay nombres para sortear");
+        resultado.textContent = "";
+        return;
+
+    } else{
+        let sortear = Math.floor(Math.random()* nombres.length); 
+        let nombreSelec = nombres [sortear];
+        document.getElementById("resultado").innerHTML = `${nombreSelec}`;
+    }    
+
+}
+
+
+
+
+function reiniciarJuego(){
+    nombres = [];
+
+    document.getElementById("listaAmigos").innerHTML="";
+
+    asignarTextoElem("h2","");
+
+    borrarCampo();
+
+    console.log( "Juego reiniciado");
+}
 
